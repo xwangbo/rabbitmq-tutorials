37e9f7ac8b56477e2191597f108bcd28ae412120
王博 <bo.wang@hexasino.com>
2018-01-19 10:45:17 +0800

    tutorial-one

--- java-mvn/src/main/java/Send.java
+++ java-mvn/src/main/java/Send.java
@@ -1,1 +1,42 @@
-../../../../java/Send.java
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
+public class Send {
+
+    public final static String QUEUE_NAME = "hello";
+    public final static String HOST = "localhost";
+
+    public static void main(String[] args) {
+        log.info("Starting");
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+
+        try (Connection connection = factory.newConnection()) {
+
+
+            log.info("connected");
+
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            String message = "Hello World!";
+
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+
+            log.info("[x] sent '{}'", message);
+
+            channel.close();
+//            connection.close();
+            log.info("disconnected");
+        } catch (TimeoutException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
